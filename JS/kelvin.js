eb96a11927cce372ca875e3d2df3ad78bddbed71
jihfan <jihfan@gmail.com>
2018-03-28 16:48:10 -0400

    First commit

---
+++ JS/kelvin.js
@@ -0,0 +1,25 @@
+var kelvin = 0;
+
+//Conversion is 283 K = 10 C = 50 F
+
+var celsius = kelvin - 273;
+
+// Celsius is 273 less than Kevin
+
+var fahrenheit = celsius*(9/5)+32;
+
+// Fahrenheit conversion
+
+var fahrenheit = Math.floor(fahrenheit);
+
+//Remove decimals
+
+/* console.log("The temperature is "+ fahrenheit + " degrees Fahrenheit"); */
+
+var newton = celsius*(33/100);
+
+// Newton conversion from Celsius
+
+var newton = Math.floor(newton);
+
+console.log(newton);
