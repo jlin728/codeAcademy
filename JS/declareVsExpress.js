a755b189319a44f778d4db5ce999a8275b215a92
jihfan <jihfan@gmail.com>
2018-03-28 20:42:13 -0400

    Third commit

--- JS/declareVsExpress.js
+++ JS/declareVsExpress.js
@@ -21,4 +21,17 @@
   };
 
   console.log(isGreaterThan(1000125, 456113));
-  
+
+/* FUNCTION EXPRESSION
+const volumeOfSphere = (diameter) => {
+  return (1/6) * Math.PI * diameter * diameter * diameter;
+}
+
+console.log('The volume of a sphere is ' + volumeOfSphere(10) + ' cubic centimeters');
+
+
+REFRACTORED concise body - removed parentheses, {}, and return is implicit */
+
+const volumeOfSphere = diameter => (1/6)*Math.PI*diameter*diameter*diameter;
+
+console.log("The volume of a sphere is " + volumeOfSphere(25) + " cubic centimeters");
